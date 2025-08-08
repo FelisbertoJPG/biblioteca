cc48fd83a246cc2e719987d51d39af8021af97d8
João Pedro Felisberto Schmidt <joaopedroschmidt@hotmail.com>
2025-08-07 21:12:08 -0300

    Funcionalidade do Loop do OP

--- Java/src/Main.java
+++ Java/src/Main.java
@@ -19,79 +19,70 @@ public class Main {
 
         //variaveis global
         int op;
+        boolean cadastrado = false;
         // do while dentro de um switch
         // dentro outras op
         // cadastros gerais e dentro dele o login
-        do {
-            System.out.println("===Biblioteca===");
-            System.out.println("Cadastre-se [1]");
-            System.out.println("Login      [2]");
-            System.out.print("Escolha: ");
-            op = scanner.nextInt();
-            if (op != 1 && op != 2) {
-                System.out.println("Opção inválida. Tente novamente.");
-            }
-        } while (op != 1 && op != 2);
 
-        switch (op){
-            case 1:
-                //Criar um Usuario Cadastrado
-                System.out.println("Insira seus dados cadastrais:");
-                System.out.println("Nome:");
-                locacao.getUsuario().setNome(scanner.nextLine());
-                locacao.getUsuario().setNome(scanner.nextLine());
-                //trata o Enter pendente scanner.nextLine();
-                //(scanner.next()) => String, para de ler quando tem Espaço na frase
-                //NextLine le a linha toda ate o Enter
-                //se perde nos sout()
-                System.out.println("Idade:");
-                locacao.getUsuario().setIdade(scanner.nextInt());
-                scanner.nextLine();
-                System.out.println("Endereço:");
-                System.out.println("----------------------");
-                System.out.println("Rua: ");
-                locacao.getUsuario().getEndereco().setRua(scanner.nextLine());
 
-                System.out.println("Bairro: ");
-                locacao.getUsuario().getEndereco().setBairro(scanner.nextLine());
 
-                System.out.println("Numero: ");
-                locacao.getUsuario().getEndereco().setNum(scanner.nextInt());
+        while (true) {
+            do {
+                System.out.println("===Biblioteca===");
+                System.out.println("Cadastre-se [1]");
+                System.out.println("Login      [2]");
+                System.out.print("Escolha: ");
+                op = scanner.nextInt();
+                scanner.nextLine(); // Consome o ENTER
+                if (op != 1 && op != 2) {
+                    System.out.println("Opção inválida. Tente novamente.");
+                }
+            } while (op != 1 && op != 2);
 
-                //TODO: EMAIL
-                System.out.println("Insira seu E-mail de Cadastro: ");
-                locacao.getUsuario().setEmail(scanner.next());
+            switch (op) {
+                case 1:
+                    // Cadastro do usuário
+                    System.out.println("Insira seus dados cadastrais:");
+                    System.out.print("Nome: ");
+                    locacao.getUsuario().setNome(scanner.nextLine());
+                    System.out.print("Idade: ");
+                    locacao.getUsuario().setIdade(scanner.nextInt());
+                    scanner.nextLine();
+                    System.out.print("Rua: ");
+                    locacao.getUsuario().getEndereco().setRua(scanner.nextLine());
+                    System.out.print("Bairro: ");
+                    locacao.getUsuario().getEndereco().setBairro(scanner.nextLine());
+                    System.out.print("Numero: ");
+                    locacao.getUsuario().getEndereco().setNum(scanner.nextInt());
+                    scanner.nextLine();
+                    System.out.print("Insira seu E-mail de Cadastro: ");
+                    locacao.getUsuario().setEmail(scanner.nextLine());
+                    System.out.print("Insira uma senha: ");
+                    locacao.getUsuario().setSenha(scanner.nextLine());
 
-                //TODO: SENHA
-                System.out.println("Insira uma senha: ");
-                locacao.getUsuario().setSenha(scanner.next());
-
-                System.out.println("Seja Bem Vindo: "+locacao.getUsuario().getNome());
-                break;
-                //TODO:criar uma função a parte, pra tentar chamar aqui
-            case 2:
-                //TODO: LOGIN
-                //pull
-                if (locacao.getUsuario()==null){
-                    System.out.println("Nenhum usuario cadastrado!!");
+                    System.out.println("Seja Bem Vindo: " + locacao.getUsuario().getNome());
+                    cadastrado = true;
+                    op=2;
                     break;
-                    //retornar pro começo do switch
-                }
-                System.out.println("Insira seus Dados de Login:");
-                System.out.print("Nome: ");
-                String nomeLogin = scanner.nextLine();
-                System.out.print("Senha: ");
-                String senhaLogin = scanner.nextLine();
-                if(locacao.getUsuario().equals(nomeLogin) && locacao.getUsuario().equals(senhaLogin)){
-                    System.out.println("Login realizado com sucesso!!");
+                case 2:
+                    if (locacao.getUsuario() == null || !cadastrado) {
+                        System.out.println("Nenhum usuário cadastrado!!");
+                        break;
+                    }
+                    System.out.println("Insira seus Dados de Login:");
+                    System.out.print("Nome: ");
+                    String nomeLogin = scanner.nextLine();
+                    System.out.print("Senha: ");
+                    String senhaLogin = scanner.nextLine();
+                    if (locacao.getUsuario().getNome().equals(nomeLogin) && locacao.getUsuario().getSenha().equals(senhaLogin)) {
+                        System.out.println("Login realizado com sucesso!!");
 
-                }else{
-                    System.out.println("Nome e/ou Senha Invalidos, Tente Novamente!!");
-                }
-                break;
-
+                    } else {
+                        System.out.println("Nome e/ou Senha Inválidos, Tente Novamente!!");
+                    }
+                    break;
+            }
         }
-
 
     }
 
